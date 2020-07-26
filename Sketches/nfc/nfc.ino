bdacd34d0afa0a04bdda4e350a36e87fe5365aaf
Odnorizhok <DimaMorgun97@gmail.com>
2020-07-26 20:54:39 +0300

    Ethernet project has been completed.

---
+++ Sketches/nfc/nfc.ino
@@ -0,0 +1,28 @@
+/*
+ * 
+ * All the resources for this project: https://www.hackster.io/Aritro
+ * Modified by Aritro Mukherjee
+ * 
+ * 
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  
+} 
