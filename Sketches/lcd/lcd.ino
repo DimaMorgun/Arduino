452ef11a1521eb628312c561fbc1c4d33a8a0b3b
Odnorizhok <dimamorgun97@gmail.com>
2020-08-22 11:12:26 +0300

    LCD example has been updated. Rewrited accroding I2C adapter.

--- Sketches/lcd/lcd.ino
+++ Sketches/lcd/lcd.ino
@@ -1,20 +1,24 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+/* I2C LCD with Arduino example code. More info: https://www.makerguides.com */
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+// Include the libraries:
+// LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
 
-void setup()
-{
-  lcd.init();                      // initialize the lcd
-  // Print a message to the LCD.
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.
+
+void setup() {
+  // Initiate the LCD:
+  lcd.init();
   lcd.backlight();
 }
 
-
-void loop()
-{
-  lcd.setCursor(1,0);
-  lcd.print("hello everyone");
-  lcd.setCursor(1,1);
-  lcd.print("konichiwaa");
+void loop() {
+  // Print 'Hello World!' on the first line of the LCD:
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("Hello World!"); // Print the string "Hello World!"
+  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("LCD tutorial");
 }
